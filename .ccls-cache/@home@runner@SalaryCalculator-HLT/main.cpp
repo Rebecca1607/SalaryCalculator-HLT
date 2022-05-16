28396b1c657b3d318a9c4cdb4db1b30722c86d78
rebecca1990 <93472100+Rebecca1607@users.noreply.github.com>
2022-05-16 13:29:50 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@SalaryCalculator-HLT/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+cout << "Welcome to the salary calculator!\n";
+
+int month, year, total, payrise;
+year = 12;
+cout <<"Hello, How much do you earn in a month? :  ";
+cin >> month;
+total = month * year;
+
+cout << "So that means you earn " << month * year << " in a year \n";
+
+
+}
